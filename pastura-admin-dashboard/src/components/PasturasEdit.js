8122ea094ed96270c2dba2f4824ad7ed1d6f328f
Milton <88519142+AyalaTon@users.noreply.github.com>
2022-10-04 21:13:28 -0300

    Start Edit

--- pastura-admin-dashboard/src/components/PasturasEdit.js
+++ pastura-admin-dashboard/src/components/PasturasEdit.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { Edit, SimpleForm, TextInput, ReferenceInput } from "react-admin";
+import { Typography, Box } from '@mui/material';
 
 const PasturasEdit = (props) => {
   return (
     <Edit title="Editar Pastura" {...props}>
       <SimpleForm>
-        <ReferenceInput source="_id" reference="pasturas" />
+        {/* <ReferenceInput source="_id" reference="pasturas" /> */}
         <TextInput disabled source="id" />
         <TextInput source="Familia" />
         <TextInput source="Especie" />
         <TextInput source="TipoVegetativo" />
         <TextInput source="RizomaEngrosado" />
-        <TextInput source="Mocollo1" />
-        <TextInput source="Mocollo2" />
+
+        <Typography variant="h6" gutterBottom>
+          Macollo
+        </Typography>
+        <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
+          <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <TextInput source="Mocollo1" fullWidth/>
+            {/* <TextInput source="first_name" isRequired fullWidth /> */}
+          </Box>
+          <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
+            <TextInput source="Mocollo2" fullWidth/>
+            {/* <TextInput source="last_name" isRequired fullWidth /> */}
+          </Box>
+        </Box>
+
         <TextInput source="ConsistenciaLigula" />
         <TextInput source="FormaLigula" />
         <TextInput source="Tamano" />
