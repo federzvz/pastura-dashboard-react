797501b6ffd484958359f3d26b0ecc3f27787b4b
Milton <88519142+AyalaTon@users.noreply.github.com>
2022-11-01 21:15:03 -0300

    FIltros en Aside

--- pastura-admin-dashboard/src/components/PasturasList2.js
+++ pastura-admin-dashboard/src/components/PasturasList2.js
@@ -1,83 +1,84 @@
 import * as React from "react";
 import { 
-    TopToolbar,
-    FilterButton,
-    CreateButton,
-    ExportButton,
-    Button,
-    TextInput,
-    FilterForm,
-    ListBase,
-    Pagination,
+    // TopToolbar,
+    // FilterButton,
+    // CreateButton,
+    // ExportButton,
+    // Button,
+    // TextInput,
+    // FilterForm,
+    // ListBase,
+    // Pagination,
     List, 
     Datagrid, 
     TextField, 
     ShowButton
 } from 'react-admin';
-import { Stack } from '@mui/material';
-import IconEvent from '@mui/icons-material/Event';
+// import { Stack } from '@mui/material';
+// import IconEvent from '@mui/icons-material/Event';
+import { PostFilterSidebar } from './PostFilterSidebar';
 
-const postFilters = [
-    <TextInput label="Search" source="q" alwaysOn />,
-    <TextInput label="Title" source="UbicacionPelos" defaultValue="Hello, World!" />,
-];
+// const postFilters = [
+//     <TextInput label="Search" source="q" alwaysOn />,
+//     <TextInput label="Title" source="UbicacionPelos" defaultValue="Hello, World!" />,
+// ];
 
-const ListActions = (props) => (
-    <TopToolbar>
-        {/* <FilterButton filters={postFilters}/> */}
-        <CreateButton/>
-        <ExportButton/>
-        {/* Add your custom actions */}
-        <Button
-            onClick={() => { alert('Your custom action'); }}
-            label="Show calendar"
-        >
-            <IconEvent/>
-        </Button>
-    </TopToolbar>
-);
+// const ListActions = (props) => (
+//     <TopToolbar>
+//         {/* <FilterButton filters={postFilters}/> */}
+//         <CreateButton/>
+//         <ExportButton/>
+//         {/* Add your custom actions */}
+//         <Button
+//             onClick={() => { alert('Your custom action'); }}
+//             label="Show calendar"
+//         >
+//             <IconEvent/>
+//         </Button>
+//     </TopToolbar>
+// );
 
-const ListToolbar = () => (
-    <Stack direction="row" justifyContent="space-between">
-        <FilterForm filters={postFilters} />
-        <div>
-            <FilterButton filters={postFilters} />
-            <CreateButton />
-        </div>
-    </Stack>
-)
+// const ListToolbar = () => (
+//     <Stack direction="row" justifyContent="space-between">
+//         <FilterForm filters={postFilters} />
+//         <div>
+//             <FilterButton filters={postFilters} />
+//             <CreateButton />
+//         </div>
+//     </Stack>
+// )
 
 const PasturasList = (props) => {
     return (
-    <List {...props}>
-        <ListToolbar />
+    <List aside={<PostFilterSidebar />} {...props} >
+        {/* <ListToolbar /> */}
         <Datagrid>
-            {/* <TextField source="id" />
-            <TextField source="Familia" />
-            <TextField source="Especie" />
+            {/* <TextField source="id" /> */}
+            {/* <TextField source="Familia" /> */}
+            {/* <TextField source="Especie" /> */}
             <TextField source="TipoVegetativo" />
             <TextField source="RizomaEngrosado" />
             <TextField source="Mocollo1" />
             <TextField source="Mocollo2" />
             <TextField source="ConsistenciaLigula" />
             <TextField source="FormaLigula" />
-            <TextField source="Tamano" />
-            <TextField source="OtrasCaracteristicasLigula" />
-            <TextField source="ColorLigula" />
+            {/* <TextField source="Tamano" /> */}
+            {/* <TextField source="OtrasCaracteristicasLigula" /> */}
+            {/* <TextField source="ColorLigula" /> */}
             <TextField source="FormaLamina" />
-            <TextField source="Canaliculada" />
+            {/* <TextField source="Canaliculada" /> */}
             <TextField source="TipoLamina" />
             <TextField source="Apice" />
             <TextField source="NervaduraCentralMarcada" />
-            <TextField source="Observaciones" />
-            <TextField source="Pelos" /> */}
-            <TextField source="UbicacionPelos" />
-            <TextField source="Observacion" />
-            <TextField source="ObservacionesGenerales" />
+            {/* <TextField source="Observaciones" /> */}
+            <TextField source="Pelos" />
+            {/* <TextField source="UbicacionPelos" /> */}
+            {/* <TextField source="Observacion" /> */}
+            {/* <TextField source="ObservacionesGenerales" /> */}
             <TextField source="CicloVida" />
             <TextField source="CicloProductivo" />
-            <TextField source="TipoProductivo" />
-            <TextField source="TipoCampo" />
+            {/* <TextField source="TipoProductivo" /> */}
+            {/* <TextField source="TipoCampo" /> */}
             <ShowButton/>
         </Datagrid>
     </List>
---
+++ pastura-admin-dashboard/src/components/PostFilterSidebar.js
@@ -0,0 +1,91 @@
+import { SavedQueriesList, FilterLiveSearch, FilterList, FilterListItem } from 'react-admin';
+import { Card, CardContent } from '@mui/material';
+import MailIcon from '@mui/icons-material/MailOutline';
+import CategoryIcon from '@mui/icons-material/LocalOffer';
+
+export const PostFilterSidebar = () => (
+    <Card sx={{ order: -1, mr: 2, mt: 9, width: 450 }}>
+        <CardContent>
+            {/* <SavedQueriesList />
+            <FilterLiveSearch /> */}
+            {/* <FilterList label="Rizoma Engrosado" icon={<MailIcon />}>
+                <FilterListItem label="Si" value={{ RizomaEngrosado: true }} />
+                <FilterListItem label="No" value={{ RizomaEngrosado: false }} />
+            </FilterList> */}
+            <FilterList label="Nervadura central marcada" icon={<MailIcon />}>
+                <FilterListItem label="Si" value={{ NervaduraCentralMarcada: true }} />
+                <FilterListItem label="No" value={{ NervaduraCentralMarcada: false }} />
+            </FilterList>
+            <FilterList label="Tipo vegetativo" icon={<MailIcon />}>
+                <FilterListItem label="Cespitoso" value={{ TipoVegetativo: 'Cespitoso' }} />
+                <FilterListItem label="Estolonifero" value={{ TipoVegetativo: 'Estolonifero' }} />
+                <FilterListItem label="Rizomatozo" value={{ TipoVegetativo: 'Rizomatozo' }} />
+                <FilterListItem label="Estolonifero-rizomatozo" value={{ TipoVegetativo: 'Estolonifero-rizomatozo' }} />
+            </FilterList>
+            <FilterList label="Macollo 1" icon={<MailIcon />}>
+                <FilterListItem label="Semi rollizo" value={{ Mocollo1: 'Semi rollizo' }} />
+                <FilterListItem label="Comprimido" value={{ Mocollo1: 'Comprimido' }} />
+                <FilterListItem label="Semi Rolliza" value={{ Mocollo1: 'Semi Rolliza' }} />
+                <FilterListItem label="Rollizo" value={{ Mocollo1: 'Rollizo' }} />
+            </FilterList>
+            <FilterList label="Macollo 2" icon={<MailIcon />}>
+                <FilterListItem label="Intravaginal" value={{ Mocollo2: 'Intravaginal' }} />
+                <FilterListItem label="Extravaginal" value={{ Mocollo2: 'Extravaginal' }} />
+                <FilterListItem label="Extravaginal principalmente" value={{ Mocollo2: 'Extravaginal principalmente' }} />
+                <FilterListItem label="Intra o extra" value={{ Mocollo2: 'Intra o extra' }} />
+                <FilterListItem label="Intravaginal o Extravaginal" value={{ Mocollo2: 'Intravaginal o Extravaginal' }} />
+                <FilterListItem label="Intravaginal o Extravaginal principalmente" value={{ Mocollo2: 'Intravaginal o Extravaginal principalmente' }} />
+            </FilterList>
+            <FilterList label="Consistencia de la Ligula" icon={<MailIcon />}>
+                <FilterListItem label="Membranacea" value={{ ConsistenciaLigula: 'Membranacea' }} />
+                <FilterListItem label="Disuelta en pelos" value={{ ConsistenciaLigula: 'Disuelta en pelos' }} />
+                <FilterListItem label="Mixta" value={{ ConsistenciaLigula: 'Mixta' }} />
+            </FilterList>
+            <FilterList label="Forma de la ligula" icon={<MailIcon />}>
+                <FilterListItem label="Truncada y dientada" value={{ FormaLigula: 'Truncada y dientada' }} />
+                <FilterListItem label="Truncada" value={{ FormaLigula: 'Truncada' }} />
+                <FilterListItem label="Aguda" value={{ FormaLigula: 'Aguda' }} />
+                <FilterListItem label="Redondeada" value={{ FormaLigula: 'Redondeada' }} />
+            </FilterList>
+            <FilterList label="Forma de la lamina" icon={<MailIcon />}>
+                <FilterListItem label="Linear" value={{ FormaLamina: 'Linear' }} />
+                <FilterListItem label="Navicular" value={{ FormaLamina: 'Navicular' }} />
+                <FilterListItem label="Linear-Lanceolada" value={{ FormaLamina: 'Linear-Lanceolada' }} />
+                <FilterListItem label="Lanceolada" value={{ FormaLamina: 'Lanceolada' }} />
+            </FilterList>
+            <FilterList label="Tipo de lamina" icon={<MailIcon />}>
+                <FilterListItem label="Plana" value={{ TipoLamina: 'Plana' }} />
+                <FilterListItem label="Navicular-Plegada" value={{ TipoLamina: 'Navicular-Plegada' }} />
+                <FilterListItem label="Navicular" value={{ TipoLamina: 'Navicular' }} />
+                <FilterListItem label="Plegada" value={{ TipoLamina: 'Plegada' }} />
+                <FilterListItem label="Navicular a plana" value={{ TipoLamina: 'Navicular a plana' }} />
+            </FilterList>
+            <FilterList label="Ápice" icon={<MailIcon />}>
+                <FilterListItem label="Agudo" value={{ Apice: 'Agudo' }} />
+                <FilterListItem label="Obtuso" value={{ Apice: 'Obtuso' }} />
+            </FilterList>
+            <FilterList label="Pelos" icon={<MailIcon />}>
+                <FilterListItem label="Glabra" value={{ Pelos: 'Glabra' }} />
+                <FilterListItem label="Pubecente" value={{ Pelos: 'Pubecente' }} />
+                <FilterListItem label="Glabra o Pubecente" value={{ Pelos: 'Glabra o Pubecente' }} />
+                <FilterListItem label="Poco a nada" value={{ Pelos: 'Poco a nada' }} />
+            </FilterList>
+            <FilterList label="Ciclo de vida" icon={<MailIcon />}>
+                <FilterListItem label="Anual" value={{ CicloVida: 'Anual' }} />
+                <FilterListItem label="Perenne" value={{ CicloVida: 'Perenne' }} />
+            </FilterList>
+            <FilterList label="Ciclo Productivo" icon={<MailIcon />}>
+                <FilterListItem label="Invernal" value={{ CicloProductivo: 'Invernal' }} />
+                <FilterListItem label="Estival" value={{ CicloProductivo: 'Estival' }} />
+            </FilterList>
+            
+            
+            {/* <FilterList label="Category" icon={<CategoryIcon />}>
+                <FilterListItem label="Tests" value={{ category: 'tests' }} />
+                <FilterListItem label="News" value={{ category: 'news' }} />
+                <FilterListItem label="Deals" value={{ category: 'deals' }} />
+                <FilterListItem label="Tutorials" value={{ category: 'tutorials' }} />
+            </FilterList> */}
+        </CardContent>
+    </Card>
+);
